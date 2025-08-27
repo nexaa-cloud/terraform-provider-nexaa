32f962a1424beb5110bbfba0ac073b3079d9e48a
Stèlios Stigter <sstigter@tilaa.com>
2025-08-27 10:30:33 +0200

    Renamed database to cloud_database_cluster_database

--- internal/resources/nexaa_cloud_database_cluster_database.go
+++ internal/resources/nexaa_cloud_database_cluster_database.go
@@ -36,7 +36,7 @@ type cloudDatabaseClusterDatabaseResource struct {
 }
 
 func (r *cloudDatabaseClusterDatabaseResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
-	resp.TypeName = req.ProviderTypeName + "_database"
+	resp.TypeName = req.ProviderTypeName + "_cloud_database_cluster_database"
 }
 
 func (r *cloudDatabaseClusterDatabaseResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
