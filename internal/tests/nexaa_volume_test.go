5a36d380b5418610c664bc0b2367e258222378c5
Stèlios Stigter <sstigter@tilaa.com>
2026-05-28 10:42:11 +0200

    Added volume test back

---
+++ internal/tests/nexaa_volume_test.go
@@ -0,0 +1,82 @@
+// Copyright Tilaa B.V. 2026
+// SPDX-License-Identifier: MPL-2.0
+
+// internal/tests/volume_test.go
+
+package tests
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func volumeConfig(namespaceName, volumeName string, size int) string {
+	return givenProvider() +
+		givenNamespace(namespaceName, "") +
+		givenVolume(volumeName, size)
+}
+
+func TestAcc_VolumeResource_basic(t *testing.T) {
+	testAccPreCheck(t)
+
+	// Generate random test data
+	namespaceName := generateTestNamespace()
+	volumeName := generateTestVolumeName()
+	initialSize := generateRandomSize()
+	updatedSize := initialSize + generateRandomSize() // Ensure updated size is different
+
+	t.Logf("=== VOLUME TEST USING NAMESPACE: %s ===", namespaceName)
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// 1) Create & Read
+			{
+				Config: volumeConfig(namespaceName, volumeName, initialSize),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("nexaa_volume.volume", "id"),
+					resource.TestCheckResourceAttr("nexaa_volume.volume", "namespace", namespaceName),
+					resource.TestCheckResourceAttr("nexaa_volume.volume", "name", volumeName),
+					resource.TestCheckResourceAttr("nexaa_volume.volume", "size", fmt.Sprintf("%d", initialSize)),
+					resource.TestCheckResourceAttrSet("nexaa_volume.volume", "usage"),
+					resource.TestCheckResourceAttrSet("nexaa_volume.volume", "locked"),
+				),
+			},
+
+			// 2) ImportState
+			{
+				ResourceName:            "nexaa_volume.volume",
+				ImportState:             true,
+				ImportStateId:           fmt.Sprintf("%s/volume/%s", namespaceName, volumeName),
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"last_updated", "status"},
+			},
+
+			// 3) Update & Read
+			{
+				Config: volumeConfig(namespaceName, volumeName, updatedSize),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("nexaa_volume.volume", "size", fmt.Sprintf("%d", updatedSize)),
+					resource.TestCheckResourceAttrSet("nexaa_volume.volume", "usage"),
+					resource.TestCheckResourceAttrSet("nexaa_volume.volume", "locked"),
+				),
+				PreConfig: func() {
+					t.Log("Waiting 10 seconds before update...")
+					time.Sleep(10 * time.Second)
+				},
+			},
+
+			{
+				Config:  volumeConfig(namespaceName, volumeName, updatedSize),
+				Destroy: true,
+				PreConfig: func() {
+					t.Log("Waiting 10 seconds before destroy...")
+					time.Sleep(10 * time.Second)
+				},
+			},
+		},
+	})
+}
